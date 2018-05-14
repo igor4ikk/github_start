1513451a2f6f9e6528f0ea8babc0d7f40ba9e326
igor4ikk <34354041+igor4ikk@users.noreply.github.com>
2018-05-14 23:58:00 +0300

    Add files via upload

---
+++ js/animate_css.js
@@ -0,0 +1,23 @@
+$(function (){
+	$('#smartfonu  article').hover(
+		function(){$(this).addClass('animated shake')},
+		function(){$(this).removeClass('animated shake')}
+		);
+});
+
+
+$(function (){
+	$('#girobordu article').hover(
+		function(){$(this).addClass('animated swing')},
+		function(){$(this).removeClass('animated swing')}
+		);
+});
+
+
+$(function (){
+	$('#notebook article').hover(
+		function(){$(this).addClass('animated swing')},
+		function(){$(this).removeClass('animated swing')}
+		);
+});
+
